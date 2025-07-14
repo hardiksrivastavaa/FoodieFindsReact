4c2b42575fd82ed02db3f51bc235584ddeab781b
hardiksrivastavaa <hardikfgp@gmail.com>
2025-07-14 19:39:30 +0530

    14.7.25

--- src/App.js
+++ src/App.js
@@ -96,5 +96,5 @@ const appRouter = createBrowserRouter([
     },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(<RouterProvider router={appRouter} />);
